722b268628958dbe4dc57620b0f029a4b19bfe63
Parth <pp03232422@student.ku.edu.np>
2024-06-04 15:40:47 +0545

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <time.h>
+#include<ctime>
 #include <cstdlib>
 #include "QuickSort.cpp"
 
+
 using namespace std;
 int main ()
-{   
+
+{  
+
 
     int size=0;
      srand(time(0)); 
@@ -13,13 +17,29 @@ int main ()
      cin>>size;
      int Array[size];
 
+    
+
   
     for (int i = 0; i < size; i++) 
         {
-            Array[i]=rand(); 
+            Array[i]=rand();
+             
         }
-   
+        auto start = std::chrono::steady_clock::now();
+    for (int i = 0; i < size; i++) 
+        {
+           cout<<Array[i]<<endl;
+             
+        }
+    quickSort(Array,0,size-1);
+    for (int i = 0; i < size; i++) 
+        {
+           cout<<Array[i]<<endl;
+             
+        }
+    
 
     return 0;
 
 }
+
